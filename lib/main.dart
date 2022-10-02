7d7e3ef391e20a641faf58738fdbaea9e737aa72
nikitapandey196 <pandeynikita196@gmail.com>
2022-10-02 10:54:59 +0530

    first_app_properties

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,21 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+       //actionsIconTheme: IconThemeData(color: Colors.black), :used to change coloue of the icon
+       elevation: 10,
+        toolbarHeight: 60,
         backgroundColor: Colors.green,
-      ),
-    );
+        title: Text("Home"), 
+        centerTitle: true,
+        leading: Icon(Icons.menu),
+        actions: [Icon(Icons.notifications),Padding(
+          padding: const EdgeInsets.only(right: 10,left: 10),
+          child: Icon(Icons.call),
+        ),Padding(
+          padding: const EdgeInsets.only(right: 10,left: 6),
+          child: Icon(Icons.mail),
+        )],//used to center the title
+        ),
+        );//scaffold is a class used to design screen 
   }
 }
